ab6bb35c73599ff1c3db9faddab98e54aae9ffe8
GPS <gregorypujolsierra@gmail.com>
2020-06-26 02:46:14 -0500

    feat: add user login/register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,10 @@
-<?php
-    $app_name = env('APP_NAME');
-?>
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title><?php echo $app_name?></title>
+        <title>{{ config('app.name') }}</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -84,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    <?php echo $app_name?>
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
